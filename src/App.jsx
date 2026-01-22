be0dd90d8b160bd883d4d3f7dc793761a2c50da3
Ana C. <artoneya1@gmail.com>
2026-01-22 09:48:38 +0100

    edit buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import "./components/main.scss";
+import Input from "./components/Input";
+import Button from "./components/Button";
 
 export default function App() {
   return (
-    <div className="app">
+    <div className="container">
       <h1>My Todo App</h1>
-      <p>Add a new task</p>
+      <div>
+        <Input title="Add a new task" />
+        <button className="button">Add task</button>
+      </div>
     </div>
   );
 }
+
+/*
+const Button = ({title}) => {
+  <button className={stylesReload.btn}>
+    {title}
+  </button>
+}
+
+const Input = ({title, id, name}) => <input id={id} name={name} className={styles.formControl} placeholder={title} />
+*/
---
+++ src/components/Input.jsx
@@ -0,0 +1,3 @@
+export default function Input({ title }) {
+  return <input placeholder={title} />;
+}
